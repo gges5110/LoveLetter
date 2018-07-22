152ef3af547ba4068d9614f95a904681f616d77b
Yu-Chia Wu <gerrywu@utexas.edu>
2018-07-22 13:12:37 -0500

    [RL] Change index representation
    - Switched from representing state index as integer to using multi dimensional vectors with math.js library
    - Add test for each of the member functions of ReinforcementAI class
    - Split ReinforcementAI test into multiple sections

--- src/reinforcementAI.js
+++ src/reinforcementAI.js
@@ -1,9 +1,10 @@
 /*
 Step 1: Initialize Q Value Table
-  ind2sub([2, 9, 8, 8, 8, 4, 7], (S + A)) => [1][5][2][4][3][1][6] (stateVector) + (actionVector)
   for (i = 0 ~ product([2, 9, 8, 8, 8, 4, 7])) {
     QValueTable[i] = Math.random();
+    math.reshape(QValueTable, [2, 9, 8, 8, 8, 4, 7])
   }
+
 Step 2: Lookup Q Value Table based on the current state, and decided what action to take
   state definition:
   stateVector = [
@@ -40,7 +41,7 @@ Step 4: Update Q Value Table (agent learning)
 
 Step 5: Repeat Step 2 until game ends/training complete
 */
-import { ind2sub } from 'ind2sub';
+import math from 'mathjs';
 
 export default class ReinforcementAI {
   constructor(stateSizeArr, actionSizeArr) {
@@ -50,31 +51,28 @@ export default class ReinforcementAI {
     this.SASizeArr = [];
     this.SASizeArr = this.SASizeArr.concat(stateSizeArr);
     this.SASizeArr = this.SASizeArr.concat(actionSizeArr);
-    this.lastSAIndex = -1;
+    this.lastSAVector = -1;
     this.alpha = 0.5;
   }
 
   initialize() {
-    let totalSALength = this.totalSALength();
-    for (let i = 0; i < totalSALength; ++i) {
-      this.QValueTable[i] = Math.random();
-    }
+    this.QValueTable = math.random(this.SASizeArr);
   }
 
   getBestAction(reduxState) {
     let SObject = this.reduxStateToSObject(reduxState);
-    let allowedSAIndicies = this.allowedSAIndicies(SObject);
-    let bestSAIndex = this.getMaxQValueSAIndexGivenSAIndicies(allowedSAIndicies);
-    this.lastSAIndex = bestSAIndex;
-    return this.SAIndexToActionObject(bestSAIndex);
+    let allowedSAVectors = this.allowedSAVectors(SObject);
+    let bestSAVector = this.getMaxQValueSAVectorGivenSAVectors(allowedSAVectors);
+    this.lastSAVector = bestSAVector;
+    return this.SAVectorToActionObject(bestSAVector);
   }
 
   learn(nextReduxState) {
     let nextSObject = this.reduxStateToSObject(nextReduxState);
-    let allowedSAIndicies = this.allowedSAIndicies(nextSObject);
-    let max_A = this.getMaxQValueGivenSAIndicies(allowedSAIndicies);
-    this.QValueTable[this.lastSAIndex] = this.QValueTable[this.lastSAIndex] +
-      this.alpha * [this.reward(nextSObject) + max_A - this.QValueTable[this.lastSAIndex]]
+    let allowedSAIndicies = this.allowedSAVectors(nextSObject);
+    let max_A = this.getMaxQValueGivenSAVectors(allowedSAIndicies);
+    this.QValueTable[this.lastSAVector] = this.QValueTable[this.lastSAVector] +
+      this.alpha * [this.reward(nextSObject) + max_A - this.QValueTable[this.lastSAVector]]
   }
 
   // Private functions
@@ -86,9 +84,10 @@ export default class ReinforcementAI {
     player0holdingCard1: reduxState.players[0].holdingCards[1],
   }
   */
-  allowedSAIndicies(SObject) {
-    let SIndex = this.SObjectToSIndex(SObject);
-    let AIndicies = [], SAIndicies = [];
+  allowedSAVectors(SObject) {
+    // Represent index as 1d vector, math.subset([[0, 1], [2, 3]], math.index(1, 0)) // 2
+    let SVector = this.SObjectToSVector(SObject);
+    let AVectors = [], SAIndicies = [];
     if (SObject.player0dead) {
       return as;
     }
@@ -104,8 +103,8 @@ export default class ReinforcementAI {
       playAgainst = [2, 3, 4];
       guess = [2, 3, 4, 5, 6, 7, 8];
     }
-    AIndicies = AIndicies.concat(this.generateActionIndicies(SObject.player0holdingCard0, playAgainst, guess));
-    let actionObjects1 = this.AIndiciesToActionObjects(AIndicies);
+    AVectors = AVectors.concat(this.generateActionVectors(SObject.player0holdingCard0, playAgainst, guess));
+    let actionObjects1 = this.AIndiciesToActionObjects(AVectors);
 
     // SObject.player0card1
     if (SObject.player0holdingCard1 === 1) {
@@ -118,9 +117,9 @@ export default class ReinforcementAI {
       playAgainst = [2, 3, 4];
       guess = [];
     }
-    AIndicies = AIndicies.concat(this.generateActionIndicies(SObject.player0holdingCard1, playAgainst, guess));
-    let actionObjects2 = this.AIndiciesToActionObjects(AIndicies);
-    return this.combineSAIndicies(SIndex, AIndicies);
+    AVectors = AVectors.concat(this.generateActionVectors(SObject.player0holdingCard1, playAgainst, guess));
+    let actionObjects2 = this.AIndiciesToActionObjects(AVectors);
+    return this.combineSAVectors(SVector, AVectors);
   }
 
   // For debugging
@@ -128,51 +127,77 @@ export default class ReinforcementAI {
     // [8, 4, 7]
     let arr = [];
     for (let i = 0; i < AIndicies.length; ++i) {
-      arr.push(this.AIndexToActionObjects(AIndicies[i]));
+      arr.push(this.AVectorToActionObject(AIndicies[i]));
     }
     return arr;
   }
 
-  AIndexToActionObjects(AIndex) {
-    let arr = ind2sub(this.actionSizeArr, AIndex);
+  AVectorToActionObject(AIndex) {
+    let arr = AIndex;
     return {
-      cardId: arr[2],
+      cardId: arr[0] + 1,
       playAgainst: arr[1] + 1,
-      guardGuess: arr[0] + 1
+      guardGuess: arr[2] + 2
     };
   }
 
-  combineSAIndicies(SIndex, AIndicies) {
-    let shiftedSIndex = SIndex + this.product(this.stateSizeArr);
+  combineSAVectors(SVector, AVectors) {
+    // [1, 2, 3, 4], [[5, 6, 7], [3, 2, 1]]
+    // return [[1, 2, 3, 4, 5, 6, 7], [1, 2, 3, 4, 3, 2, 1]]
     let ret = [];
-    for (let i = 0; i < AIndicies.length; ++i) {
-      ret.push(AIndicies[i] + shiftedSIndex);
+    for (let i = 0; i < AVectors.length; ++i) {
+      let combinedSAVector = SVector;
+      combinedSAVector = combinedSAVector.concat(AVectors[i]);
+      ret.push(combinedSAVector);
     }
     return ret;
   }
 
-  generateActionIndicies(cardIdx, playAgainstArr, guessArr) {
+  generateActionVectors(cardIdx, playAgainstArr, guessArr) {
     let arr = [];
     // [8, 4, 7]
     for (var pidx = 0; pidx < playAgainstArr.length; ++pidx) {
       for (var gidx = 0; gidx < guessArr.length; ++gidx) {
-        arr.push((cardIdx * 4 + playAgainstArr[pidx]) * 7 + guessArr[gidx]);
+        let actionVector = [];
+        actionVector.push(cardIdx - 1);
+        actionVector.push(playAgainstArr[pidx] - 1);
+        actionVector.push(guessArr[gidx] - 2);
+        arr.push(actionVector);
       }
     }
     return arr;
   }
 
-  getMaxQValueSAIndexGivenSAIndicies(SAIndicies) {
-    if (SAIndicies.length === undefined || SAIndicies.length === 0) {
+  convertSAVectorToMathIndex(SAVector) {
+    return math.index(SAVector[0], SAVector[1], SAVector[2], SAVector[3], SAVector[4], SAVector[5], SAVector[6]);
+  }
+
+  getQValue(SAVector) {
+    return math.subset(this.QValueTable, this.convertSAVectorToMathIndex(SAVector));
+  }
+
+  getMaxQValueSAVectorGivenSAVectors(SAVectors) {
+    if (SAVectors.length === undefined || SAVectors.length === 0) {
       return -1;
     } else {
-      let max = this.QValueTable[SAIndicies[0]], index = SAIndicies[0];
-      for (let i = 1; i < SAIndicies.length; ++i) {
-        max = Math.max(max, this.QValueTable[SAIndicies[i]]);
-        index = SAIndicies[i];
+      let max = this.getQValue(SAVectors[0]), vector = SAVectors[0];
+      for (let i = 1; i < SAVectors.length; ++i) {
+        if (max < this.getQValue(SAVectors[i])) {
+          max = this.getQValue(SAVectors[i]);
+          vector = SAVectors[i];
+        }
       }
-      return index;
+      return vector;
     }
+  }
+
+  getMaxQValueGivenSAVectors(SAVectors) {
+    let vector = this.getMaxQValueSAVectorGivenSAVectors(SAVectors);
+    return this.getQValue(vector);
+  }
+
+  getAVectorFromSAVector(SAVector) {
+    return math.subset(SAVector, math.index(math.range(this.stateSizeArr.length, this.stateSizeArr.length + this.actionSizeArr.length)));
   }
 
   reduxStateToSObject(reduxState) {
@@ -192,7 +217,7 @@ export default class ReinforcementAI {
 
     return {
       player0dead: reduxState.players[1].dead,
-      player0lastCardId: player0lastCardId.cardId,
+      player0lastCardId: reduxState.players[1].playedCards[0].cardId,
       player0holdingCard0: reduxState.players[1].holdingCards[0],
       player0holdingCard1: reduxState.players[1].holdingCards[1],
     }
@@ -202,14 +227,9 @@ export default class ReinforcementAI {
     return SObject.player0dead === true ? -100 : 0;
   }
 
-  SAIndexToActionObject(SAIndex) {
-    // ind2sub([2, 9, 8, 8, 8, 4, 7], (S + A)) => [1][5][2][4][3][1][6] (stateVector) + (actionVector)
-    let arr = ind2sub(this.SASizeArr, SAIndex);
-    return {
-      cardId: arr[this.stateSizeArr.length + 2],
-      playAgainst: arr[this.stateSizeArr.length + 1] + 1,
-      guardGuess: arr[this.stateSizeArr.length] + 1
-    };
+  SAVectorToActionObject(SAVector) {
+    let AIndex = this.getAVectorFromSAVector(SAVector);
+    return this.AVectorToActionObject(AIndex);
   }
 
   SAVectorToInteger(S, A) {
@@ -234,37 +254,5 @@ export default class ReinforcementAI {
     ret.push(SObject.player0holdingCard0);
     ret.push(SObject.player0holdingCard1);
     return ret;
-  }
-
-  SObjectToSIndex(SObject) {
-    let SVector = this.SObjectToSVector(SObject);
-    let ret = 0;
-    for (let i = 0; i < this.stateSizeArr.length; ++i) {
-      if (i !== 0) {
-        ret *= this.stateSizeArr[i];
-      }
-      ret += SVector[i];
-    }
-    return ret;
-  }
-
-  getMaxQValueGivenSAIndicies(SAIndicies) {
-    let index = this.getMaxQValueSAIndexGivenSAIndicies(SAIndicies);
-    return this.QValueTable[index];
-  }
-
-  totalSALength() {
-    let totalSAArr = [];
-    totalSAArr = totalSAArr.concat(this.stateSizeArr);
-    totalSAArr = totalSAArr.concat(this.actionSizeArr);
-    return this.product(totalSAArr);
-  }
-
-  product(arr) {
-    let retVal = 1;
-    for (let i = 0; i < arr.length; ++i) {
-      retVal *= arr[i];
-    }
-    return retVal;
   }
 }
--- test/reinforcementAI.js
+++ test/reinforcementAI.js
@@ -1,22 +1,116 @@
-import { ind2sub } from 'ind2sub';
 import chai from 'chai';
+import ReinforcementAI from '../src/reinforcementAI';
+import { initialState } from '../src/const';
+import math from 'mathjs';
 
 let expect = chai.expect;
 
-// np.unravel_index([22, 41, 37], (7,6))
-// (array([3, 6, 6]), array([4, 5, 1]))
+describe('ReinforcementAI.public functions', () => {
+  it('ReinforcementAI.initialize', () => {
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    reinforcementAI.initialize();
+    expect(reinforcementAI.QValueTable.length).to.equal(reinforcementAI.SASizeArr[0]);
+    expect(reinforcementAI.QValueTable[0].length).to.equal(reinforcementAI.SASizeArr[1]);
+    expect(reinforcementAI.QValueTable[0][0].length).to.equal(reinforcementAI.SASizeArr[2]);
+  })
 
-describe('General Reducer', () => {
-  it('should return the initial state'
-    , () => {
-      expect(ind2sub([2, 2], 2)).to.deep.equal([0, 1]);
-      expect(ind2sub([7, 4, 8], 124)).to.equal([5, 1, 4]);
-    }
-  );
+  it('ReinforcementAI.getBestAction', () => {
+    let state = JSON.parse(JSON.stringify(initialState));
+    state.players[1].playedCards.push({
+      cardId: 3
+    });
+    state.players[1].dead = false;
+    state.players[1].holdingCards.push(1);
+    state.players[1].holdingCards.push(1);
 
-  it('should return the initial state1'
-    , () => {
-      expect(ind2sub([22, 41, 37], 7)).to.equal([3, 6, 6]);
-    }
-  );
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    reinforcementAI.initialize();
+
+    let card = reinforcementAI.getBestAction(state);
+    console.log(card);
+    expect(card.cardId).to.equal(1);
+    expect(card.playAgainst).to.be.oneOf([2, 3, 4]);
+    expect(card.guardGuess).to.be.oneOf([2, 3, 4, 5, 6, 7, 8]);
+  })
+});
+
+
+describe('ReinforcementAI.private functions', () => {
+  it('SObjectToSVector', () => {
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    let SVector = reinforcementAI.SObjectToSVector({
+      player0dead: true,
+      player0lastCardId: 6,
+      player0holdingCard0: 5,
+      player0holdingCard1: 0
+    });
+
+    expect(SVector).to.deep.equal([1, 6, 5, 0]);
+    // expect(reinforcementAI.totalSALength()).to.equal(258048);
+  })
+
+  it('generateActionVectors', () => {
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    // let SVector = [1, 6, 5, 0];
+    let player0holdingCard0 = 8;
+    let playAgainst = [2, 3, 4];
+    let guess = [2, 3, 4, 5, 6, 7, 8];
+    let AVectors = reinforcementAI.generateActionVectors(player0holdingCard0, playAgainst, guess);
+    expect(AVectors.length).to.equal(playAgainst.length * guess.length);
+    expect(AVectors[0]).to.deep.equal([player0holdingCard0 - 1, playAgainst[0] - 1, guess[0] - 2]);
+    expect(AVectors[1]).to.deep.equal([player0holdingCard0 - 1, playAgainst[0] - 1, guess[1] - 2]);
+  })
+
+  it('AVectorToActionObject', () => {
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    // guardGuess [2, 3, 4, 5, 6, 7, 8]
+    // action vec [0, 1, 2, 3, 4, 5, 6]
+    let actionObject = reinforcementAI.AVectorToActionObject([0, 2, 6]);
+    expect(actionObject.cardId).to.equal(1);
+    expect(actionObject.playAgainst).to.equal(3);
+    expect(actionObject.guardGuess).to.equal(8);
+  });
+
+  it('getMaxQValueSAVectorGivenSAVectors', () => {
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    reinforcementAI.initialize();
+    let SAVectors = [
+      [0, 0, 0, 0, 0, 2, 5],
+      [0, 0, 0, 0, 0, 2, 6],
+    ]
+    let SAVector = reinforcementAI.getMaxQValueSAVectorGivenSAVectors(SAVectors);
+    expect(SAVector).to.deep.be.oneOf(SAVectors);
+  });
+
+  it('combineSAVectors', () => {
+    let SVector = [1, 6, 5, 1];
+    let AVectors = [[2, 3, 4], [1, 2, 3]];
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    expect(reinforcementAI.combineSAVectors(SVector, AVectors)).to.deep.equal([[1, 6, 5, 1, 2, 3, 4], [1, 6, 5, 1, 1, 2, 3]]);
+  })
+
+  it('SAVectorToActionObject', () => {
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    expect(reinforcementAI.SAVectorToActionObject([0, 0, 0, 0, 0, 2, 5])).to.deep.equal({
+      cardId: 1,
+      playAgainst: 3,
+      guardGuess: 7
+    });
+  })
+});
+
+describe('ReinforcementAI.utility functions', () => {
+  it('mathjs.subset', () => {
+    const b = [[0, 1], [2, 3]];
+    expect(math.subset(b, math.index(1, 0))).to.deep.equal(2);
+    let reinforcementAI = new ReinforcementAI([2, 9, 8, 8], [8, 4, 7]);
+    reinforcementAI.initialize();
+    let SAVectors = [
+      [0, 0, 0, 0, 0, 2, 6],
+      [0, 0, 0, 0, 0, 2, 7],
+    ]
+    // There's got to be a better way of doing this...
+    expect(math.subset(reinforcementAI.QValueTable, reinforcementAI.convertSAVectorToMathIndex(SAVectors[0]))).to.lessThan(1);
+    expect(math.subset(reinforcementAI.QValueTable, reinforcementAI.convertSAVectorToMathIndex(SAVectors[0]))).to.greaterThan(0);
+  })
 });
